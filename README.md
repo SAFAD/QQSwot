c7f31a28812af6c25dbb4065414f19551f2bad04
SAFAD <safad.line@gmail.com>
2016-09-16 15:03:48 +0100

    Hello World!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # QQSwot
 Simple SWOT built on VueJS
+This project is *currently intended for learning purposes only*
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-var Vue = require('vue');
+
+
+//now we need an input component
+
+var QQInput =  Vue.extend({
+  template: '#QQInput-template',
+  data: function(){
+    return {
+      newInput: '',
+      inputs: [
+        { text: 'Add some input' }
+      ]
+    }
+  } ,
+  methods: {
+    addInput: function () {
+      var text = this.newInput.trim()
+      if (text) {
+        this.inputs.push({ text: text })
+        this.newInput = ''
+      }
+    },
+    removeInput: function (index) {
+      this.inputs.splice(index, 1)
+    }
+  }
+});
+Vue.component('qqinput', QQInput);
+//need a couple of instances of this
+new Vue({
+  el: "#strengths"
+});
+new Vue({
+  el: "#weaknesses"
+});
+new Vue({
+  el: "#opportunities"
+});
+new Vue({
+  el: "#threats"
+});
